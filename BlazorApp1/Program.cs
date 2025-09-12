7169a8181ed8b2f2eeecd7eae37f72558100d3cd
YatiWaiTun86 <yatiwaitun86@gmail.com>
2025-09-12 13:09:42 +0630

    Add initial files

---
+++ BlazorApp1/Program.cs
@@ -0,0 +1,13 @@
+using BlazorApp1;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+//Create Default Blazor WebAssembly Host
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:4000") });
+
+await builder.Build().RunAsync();
